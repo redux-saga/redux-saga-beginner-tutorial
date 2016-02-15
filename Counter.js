9761eb41aa66d2f204bd50b44c613eaef871636c
yelouafi <yelouafi@gmail.com>
2016-02-15 02:55:00 +0000

    initial commit

---
+++ Counter.js
@@ -0,0 +1,23 @@
+/*eslint-disable no-unused-vars */
+import React, { Component, PropTypes } from 'react'
+
+const Counter = ({ value, onIncrement, onDecrement }) =>
+      <p>
+        Clicked: {value} times
+        {' '}
+        <button onClick={onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          -
+        </button>
+      </p>
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
---
+++ main.js
@@ -0,0 +1,25 @@
+import "babel-polyfill"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+
+import Counter from './Counter'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+const action = type => store.dispatch({type})
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
