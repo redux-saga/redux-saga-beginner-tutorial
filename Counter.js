208971e6aa3605bb431058323b9dd4403b46c954
yelouafi <yelouafi@gmail.com>
2016-02-17 11:27:34 +0000

    adapted to match the tutorial

--- Counter.js
+++ Counter.js
@@ -2,17 +2,19 @@
 import React, { Component, PropTypes } from 'react'
 
 const Counter = ({ value, onIncrement, onDecrement }) =>
-      <p>
-        Clicked: {value} times
-        {' '}
+      <div>
         <button onClick={onIncrement}>
-          +
+          Increment
         </button>
         {' '}
         <button onClick={onDecrement}>
-          -
+          Decrement
         </button>
-      </p>
+        <hr />
+        <div>
+          Clicked: {value} times
+        </div>
+      </div>
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
