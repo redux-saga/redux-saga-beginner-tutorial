76deac8c2346de6a61c9cacd5d72c901b30cbe96
Yassine Elouafi <yelouafi@gmail.com>
2016-05-21 13:09:20 +0100

    Use library provided 'delay'

--- sagas.js
+++ sagas.js
@@ -1,11 +1,9 @@
-import { takeEvery } from 'redux-saga'
+import { delay, takeEvery } from 'redux-saga'
 import { call, put } from 'redux-saga/effects'
 
 export function* helloSaga() {
   console.log('Hello Saga!')
 }
-
-export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export function* incrementAsync() {
   yield call(delay, 1000)
