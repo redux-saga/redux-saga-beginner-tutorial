4a4ef2b314dddf5113de7e1fd3bf4f52ad498355
mentorvikash <mentorvikash@gmail.com>
2022-12-16 00:28:17 +0530

    Learing redux saga

--- Counter.js
+++ Counter.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { PropTypes } from 'prop-types';
 
-const Counter = ({ value, onIncrement, onDecrement }) =>
-      <div>
-        <button onClick={onIncrement}>
-          Increment
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          Decrement
-        </button>
-        <hr />
-        <div>
-          Clicked: {value} times
-        </div>
-      </div>
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
+  <div>
+    <button onClick={onIncrementAsync}>
+      Increment after 1 second
+    </button>
+    <button onClick={onIncrement}>
+      Increment
+    </button>
+    {' '}
+    <button onClick={onDecrement}>
+      Decrement
+    </button>
+    <hr />
+    <div>
+      Clicked: {value} times
+    </div>
+  </div>
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
--- main.js
+++ main.js
@@ -3,20 +3,43 @@ import "@babel/polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from "redux-saga"
+import { put, take, takeEvery } from 'redux-saga/effects'  
 
 import Counter from './Counter'
 import reducer from './reducers'
+import {helloWorld} from './hello'
 
-const store = createStore(reducer)
+const sagaMiddlewar = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddlewar))
+sagaMiddlewar.run(helloWorld);
 
 const action = type => store.dispatch({type})
+
+const delay = (ms)=>{ new Promise(res=> setTimeout(res,ms))}
+
+// incrementAsync task to perform
+export function* incrementAsync(){
+  yield delay(1000);
+  yield put({type: 'INCREMENT'})
+}
+
+
+// Watcher
+INCREMENT_ASYNC
+export function* watchIncrementAsync(){
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} 
+    />,
+      
     document.getElementById('root')
   )
 }
